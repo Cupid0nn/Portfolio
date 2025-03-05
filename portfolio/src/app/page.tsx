b212e00ed59d4fc5bae9a462a1f91877d576ee91
Cupid0nn <malaucierno@gmail.com>
2025-03-05 10:46:22 -0300

    ahora si empezando con los divs

---
+++ portfolio/src/app/page.tsx
@@ -0,0 +1,17 @@
+import styles from "./page.module.css";
+
+
+export default function Home() {
+  return (
+    <div className="container">
+    <div className="caja1">titulo</div>
+    <div className="caja2">carrusel</div>
+    <div className="caja3">foto</div>
+    <div className="caja4">biografia</div>
+    <div className="caja5">tecnologias</div>
+    <div className="caja6">contacto</div>
+    <div className="caja7">mas contactos</div>
+    </div>
+  
+  );
+}
