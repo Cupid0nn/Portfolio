c1673ef94d211ad1faffa672c4f9e3f7224d28f4
Cupid0nn <malaucierno@gmail.com>
2025-03-06 13:42:00 -0300

    ya en camino

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,17 +1,30 @@
-import styles from "./page.module.css";
-
-
+import Image from "next/image";
 export default function Home() {
   return (
     <div className="container">
-    <div className="caja1">titulo</div>
-    <div className="caja2">carrusel</div>
-    <div className="caja3">foto</div>
-    <div className="caja4">biografia</div>
-    <div className="caja5">tecnologias</div>
-    <div className="caja6">contacto</div>
-    <div className="caja7">mas contactos</div>
+      <div className="caja1">
+        <h1>Mateo Acierno</h1>
+        <h1>Backend Dev</h1>
+      </div>
+      <div className="caja2">carrusel</div>
+      <div className="caja3">foto</div>
+      <div className="caja4">
+        <h1>
+          I'm Mateo, a passionate backend developer dedicated to building robust
+          and efficient web applications. With a deep curiosity for the
+          mechanics behind the scenes, I have specialized in backend development
+          since entering the tech world six months ago. Based in Argentina, I
+          enjoy crafting seamless digital experiences and aspire to explore AI
+          development in the future.
+        </h1>
+      </div>
+      <div className="caja5">tecnologias</div>
+      <div className="caja6">contacto</div>
+      <div className="caja7">
+        <Image src="/githubb.jpg" alt="Github" width={50} height={50} className="imagencaja7" />
+        <Image src="/linkedin.jpg" alt="Linkedin" width={50} height={50} className="imagencaja7" />
+        <Image src="/instagram.jpg" alt="Gmail" width={50} height={50} className="imagencaja7" />
+      </div>
     </div>
-  
   );
 }
