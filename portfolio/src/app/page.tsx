2f2d4a3287b090c7928a6e81613dc5b593d899f3
Cupid0nn <malaucierno@gmail.com>
2025-03-08 21:12:54 -0300

    sabadoski

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -10,7 +10,15 @@ export default function Home() {
         <h1>Backend Dev</h1>
       </div>
       <div className="caja2">carrusel</div>
-      <div className="caja3"><Image src="/perfil.jpg" alt="Picture of me" width={1280} height={490} className="imagencaja3" /> </div>
+      <div className="caja3">
+        <Image
+          src="/perfil.jpg"
+          alt="Picture of me"
+          width={1280}
+          height={490}
+          className="imagencaja3"
+        />{" "}
+      </div>
       <div className="caja4">
         <h1>
           I'm Mateo, a passionate backend developer dedicated to building robust
@@ -22,11 +30,33 @@ export default function Home() {
         </h1>
       </div>
       <div className="caja5">tecnologias</div>
-      <div className="caja6">contacto</div>
+      <div className="caja6">
+        <h1 >Some Questions?</h1>
+        <a href="mailto:malaucierno@gmail" >Know me better</a>
+      </div>
+
       <div className="caja7">
-        <Image src="/githubb.jpg" alt="Github" width={50} height={50} className="imagencaja7" />
-        <Image src="/linkedin.jpg" alt="Linkedin" width={50} height={50} className="imagencaja7" />
-        <Image src="/instagram.jpg" alt="Gmail" width={50} height={50} className="imagencaja7" />
+        <Image
+          src="/githubb.jpg"
+          alt="Github"
+          width={50}
+          height={50}
+          className="imagencaja7"
+        />
+        <Image
+          src="/linkedin.jpg"
+          alt="Linkedin"
+          width={50}
+          height={50}
+          className="imagencaja7"
+        />
+        <Image
+          src="/instagram.jpg"
+          alt="Gmail"
+          width={50}
+          height={50}
+          className="imagencaja7"
+        />
       </div>
     </div>
   );
